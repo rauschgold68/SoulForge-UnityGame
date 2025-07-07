da16d404ae2357ef7f9e1396426ab1040d5330d0
Luke Berger <luke.berger@student.uni-luebeck.de>
2025-07-07 10:49:21 +0200

    - Added combat animations and controller for LordOfDarkness and other enemies. - Also fixed animation of LightA and LightB from RF Castle

--- SoulForge_Game/Assets/Warrior free set/PlayerCombat.cs
+++ SoulForge_Game/Assets/Warrior free set/PlayerCombat.cs
@@ -4,7 +4,10 @@ public class PlayerCombat : MonoBehaviour
 {
 
     public Animator animator; // Reference to the Animator component
-    // Update is called once per frame
+
+    public Transform attackPoint; // Point from where the attack originates
+    public float attackRange = 0.5f; // Range of the attack
+    public LayerMask enemyLayers; // Layers that represent enemies
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
@@ -12,10 +15,31 @@ public class PlayerCombat : MonoBehaviour
             Attack();
         }
     }
-    
+
     void Attack()
     {
         // Implement attack logic here
         animator.SetTrigger("Attack");
+
+        // Check for enemies in the attack range
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            // Here you can implement what happens to the enemy when hit
+            // For example, you could call a method on the enemy script to apply damage
+            Debug.Log("Hit " + enemy.name);
+            // enemy.GetComponent<Enemy>().TakeDamage(damageAmount); // Assuming an Enemy script exists with a TakeDamage method
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Draw a sphere in the editor to visualize the attack range
+        if (attackPoint != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+        }
     }
 }
