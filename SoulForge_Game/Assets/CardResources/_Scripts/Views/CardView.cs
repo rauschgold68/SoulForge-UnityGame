0a488e524bc26b2b137a9ab69ca6c7bd9d08e44e
nicprp <niclas.prapavat@student.uni-luebeck.de>
2025-07-09 16:46:56 +0200

    added card templates

---
+++ SoulForge_Game/Assets/CardResources/_Scripts/Views/CardView.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CardView : MonoBehaviour
+{
+    [SerializeField] private TMP_Text description;
+
+    [SerializeField] private TMP_Text title;
+
+    [SerializeField] private SpriteRenderer imageSR;
+
+    [SerializeField] private GameObject wrapper;
+}
