4b28a381bccf63ad411ab79c295282328984c5b5
Niclas Prapavat <niclas.prapavat@student.uni-luebeck.de>
2025-07-11 15:21:36 +0200

    added card effects

--- SoulForge_Game/Assets/CardResources/_Scripts/Views/CardView.cs
+++ SoulForge_Game/Assets/CardResources/_Scripts/Views/CardView.cs
@@ -24,36 +24,38 @@ public class CardView : MonoBehaviour
     }
 
     public void Setup(Card card, PlayerMovement player = null)
-{
-    Card = card;
-    title.text = card.Title;
-    description.text = card.Description;
-    imageSR.sprite = card.Image;
+    {
+        Card = card;
+        title.text = card.Title;
+        description.text = card.Description;
+        imageSR.sprite = card.Image;
 
-    float targetHeight = 1.25f;
-    float spriteHeight = card.Image.bounds.size.y;
-    float scale = targetHeight / spriteHeight;
-    imageSR.transform.localScale = new Vector3(scale, scale, 1f);
+        float targetHeight = 1.25f;
+        float spriteHeight = card.Image.bounds.size.y;
+        float scale = targetHeight / spriteHeight;
+        imageSR.transform.localScale = new Vector3(scale, scale, 1f);
 
-    playerController = player; // <- damit `ScaleUpAndDisappear()` den Spieler wieder freigeben kann
-}
+        playerController = player; // <- damit `ScaleUpAndDisappear()` den Spieler wieder freigeben kann
+    }
 
 
 
     public void OnClickedByManager()
-{
-    // Genau das gleiche wie OnMouseDown bisher
-    if (gameObject.activeInHierarchy)
     {
-        Debug.Log("Card clicked: " + Card.Title);
+        // Genau das gleiche wie OnMouseDown bisher
+        if (gameObject.activeInHierarchy)
+        {
+            Debug.Log("Card clicked: " + Card.Title);
 
-        GameManager.RemoveCardFromStack(Card);
+            GameManager.RemoveCardFromStack(Card);
 
-        HandView.AnimateAndHideOtherCards(this, GameManager);
-        HandView.RemoveCard(this);
-        StartCoroutine(ScaleUpAndDisappear());
+            ApplyCardEffect(Card.Title);
+
+            HandView.AnimateAndHideOtherCards(this, GameManager);
+            HandView.RemoveCard(this);
+            StartCoroutine(ScaleUpAndDisappear());
+        }
     }
-}
 
 
     private IEnumerator ScaleUpAndDisappear()
@@ -63,10 +65,89 @@ public class CardView : MonoBehaviour
         yield return transform.DOScale(0f, 0.5f).WaitForCompletion();
         gameObject.SetActive(false);
         if (playerController != null)
-{
-    playerController.SetMovementEnabled(true); // <-- funktioniert nur, wenn es diese Methode gibt
-}
+        {
+            playerController.SetMovementEnabled(true); // <-- funktioniert nur, wenn es diese Methode gibt
+        }
 
 
     }
+    
+    private void ApplyCardEffect(string cardName)
+{
+    if (playerController == null)
+    {
+        Debug.LogWarning("Kein PlayerController gefunden!");
+        return;
+    }
+
+    var cardController = playerController.GetComponent<CardController>();
+    if (cardController == null)
+    {
+        Debug.LogWarning("CardController nicht am Spieler gefunden!");
+        return;
+    }
+
+    switch (cardName)
+    {
+        // Health Upgrades
+        case "HealthCard1":
+            cardController.AddHealthUpgrade("FirstUpgrade");
+            break;
+        case "HealthCard2":
+            cardController.AddHealthUpgrade("SecondUpgrade");
+            break;
+        case "HealthCard3":
+            cardController.AddHealthUpgrade("ThirdUpgrade");
+            break;
+
+        // Speed Upgrades
+        case "SpeedCard1":
+            cardController.AddSpeedUpgrade("FirstUpgrade");
+            break;
+        case "SpeedCard2":
+            cardController.AddSpeedUpgrade("SecondUpgrade");
+            break;
+        case "SpeedCard3":
+            cardController.AddSpeedUpgrade("ThirdUpgrade");
+            break;
+
+        // Damage Upgrades
+        case "DamageCard1":
+            cardController.AddDamageUpgrade("FirstUpgrade");
+            break;
+        case "DamageCard2":
+            cardController.AddDamageUpgrade("SecondUpgrade");
+            break;
+        case "DamageCard3":
+            cardController.AddDamageUpgrade("ThirdUpgrade");
+            break;
+
+        // Attack Range Upgrades
+        case "RangeCard1":
+            cardController.AddAttackRangeUpgrade("FirstUpgrade");
+            break;
+        case "RangeCard2":
+            cardController.AddAttackRangeUpgrade("SecondUpgrade");
+            break;
+        case "RangeCard3":
+            cardController.AddAttackRangeUpgrade("ThirdUpgrade");
+            break;
+
+            // Attack Speed Upgrades
+        case "AttSpeedCard1":
+            cardController.AddAttackSpeedUpgrade("FirstUpgrade");
+            break;
+        case "AttSpeedCard2":
+            cardController.AddAttackSpeedUpgrade("SecondUpgrade");
+            break;
+        case "AttSpeedCard3":
+            cardController.AddAttackSpeedUpgrade("ThirdUpgrade");
+            break;
+
+        default:
+            Debug.LogWarning("Kein Effekt für Karte: " + cardName);
+            break;
+    }
+}
+
 }
