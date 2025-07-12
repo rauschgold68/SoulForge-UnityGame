37eeaf7ada179efc163c7ccf61ff2499217e3779
Luke Berger <luke.berger@student.uni-luebeck.de>
2025-07-12 18:18:26 +0200

    Add SoulStore class for card deck upgrades and soul management

---
+++ SoulForge_Game/Assets/Scripts/GameScripts/SoulStore.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class SoulStore : MonoBehaviour
+{
+    public int cardDeckUpgrade1Cost = 50;
+    public int cardDeckUpgrade2Cost = 200;
+
+    private StatController statController;
+    // private CardController cardController;
+
+    void Awake()
+    {
+        statController = FindFirstObjectByType<StatController>();
+        // cardController = FindFirstObjectByType<CardController>();
+    }
+
+    private int GetCurrentSouls()
+    {
+        return statController != null ? statController.SoulAmount : 0;
+    }
+
+    public bool CanAfford(int cost)
+    {
+        return GetCurrentSouls() >= cost;
+    }
+
+    public bool BuyCardDeckUpgrade1()
+    {
+        if (CanAfford(cardDeckUpgrade1Cost))
+        {
+            statController.SoulAmount -= cardDeckUpgrade1Cost;
+            Debug.Log("Card Deck Upgrade 1 purchased.");
+            // cardController.UpgradeCardDeck();
+            return true;
+        }
+        return false;
+    }
+
+    public bool BuyCardDeckUpgrade2()
+    {
+        if (CanAfford(cardDeckUpgrade2Cost))
+        {
+            statController.SoulAmount -= cardDeckUpgrade2Cost;
+            Debug.Log("Card Deck Upgrade 2 purchased.");
+            // cardController.UpgradeCardDeck();
+            return true;
+        }
+        return false;
+    }
+}
